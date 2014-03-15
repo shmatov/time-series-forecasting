3fa82e48052705a660f101b5aa29b9f67d1d4533
Roman Shmatov <romanshmatov@gmail.com>
2014-03-15 14:02:11 +0600

    Исправил алгоритм усреднения

--- TimeSeriesCollection/TimeSeriesExtensions.cs
+++ TimeSeriesCollection/TimeSeriesExtensions.cs
@@ -6,9 +6,9 @@ namespace TimeSeriesCollection
 {
     public static class TimeSeriesExtensions
     {
-        public static IEnumerable<double?> Average(this IEnumerable<double?> series)
+        public static IEnumerable<double?> Average(this IEnumerable<double?> series, int windowSize = 3)
         {
-            return new SMACalculator(series).Calculate();
+            return new SMACalculator(series, windowSize).Calculate();
         }
 
         public static IEnumerable<double?> Interpolate(this IEnumerable<double?> series)
@@ -16,53 +16,87 @@ namespace TimeSeriesCollection
             throw new NotImplementedException();
         }
 
-        public static IEnumerable<double> CalculateF(this IEnumerable<double> series, IEnumerable<int> specialPointsIndices)
+        public static IEnumerable<double> CalculateF(this IEnumerable<double> series,
+            IEnumerable<int> specialPointsIndices, int radius = 3)
         {
-            return new SpecialPointsAverageCalculator(series, specialPointsIndices).Calculate();
+            return new SpecialPointsAverageCalculator(series, specialPointsIndices, radius).Calculate();
         }
     }
 
     internal class SMACalculator
     {
-        private readonly int _movingWindowLength;
+        private readonly int _movingWindowSize;
         private readonly IEnumerable<double?> _series;
 
-        public SMACalculator(IEnumerable<double?> series)
+        public SMACalculator(IEnumerable<double?> series, int movingWindowSize)
         {
             _series = series;
-            _movingWindowLength = 3;
+            _movingWindowSize = movingWindowSize;
         }
 
         public IEnumerable<double?> Calculate()
         {
-            var movingWindow = new LinkedList<double?>();
+            var movingWindow = new MovingWindow(_movingWindowSize);
             return _series.Aggregate(new List<double?>(),
                 (results, element) =>
                 {
-                    movingWindow.AddLast(element);
-                    if (movingWindow.Count > _movingWindowLength) movingWindow.RemoveFirst();
-
-                    var average = element.HasValue
-                        ? (double?) movingWindow.Where(x => x.HasValue).Average(x => x.Value)
-                        : null;
-
-                    results.Add(average);
+                    movingWindow.Add(element);
+                    results.Add(element.HasValue ? (double?) movingWindow.Average : null);
                     return results;
                 });
+        }
+
+        private class MovingWindow
+        {
+            private readonly int _size;
+            private int _hasValueCount;
+            private double _sum;
+            private readonly Queue<double?> _queue = new Queue<double?>();
+
+            public double Average
+            {
+                get { return _sum/_hasValueCount; }
+            }
+
+            public MovingWindow(int size)
+            {
+                _size = size;
+            }
+
+            public void Add(double? element)
+            {
+                _queue.Enqueue(element);
+                if (element.HasValue)
+                {
+                    _hasValueCount++;
+                    _sum += element.Value;
+                }
+
+                if (_queue.Count > _size)
+                {
+                    element = _queue.Dequeue();
+                    if (element.HasValue)
+                    {
+                        _hasValueCount--;
+                        _sum -= element.Value;
+                    }
+                }
+            }
         }
     }
 
     internal class SpecialPointsAverageCalculator
     {
-        private readonly int[] _offsets;
+        private readonly List<int> _offsets;
         private readonly List<double> _series;
         private readonly IEnumerable<int> _specialPointsIndices;
 
-        public SpecialPointsAverageCalculator(IEnumerable<double> series, IEnumerable<int> specialPointsIndices)
+        public SpecialPointsAverageCalculator(IEnumerable<double> series, IEnumerable<int> specialPointsIndices,
+            int radius)
         {
             _series = series.ToList();
             _specialPointsIndices = specialPointsIndices;
-            _offsets = Enumerable.Range(-1*5, 5).ToArray();
+            _offsets = Enumerable.Range(-1*radius, radius).ToList();
         }
 
         private IEnumerable<IEnumerable<int>> SpecialPointsRegions
@@ -72,7 +106,7 @@ namespace TimeSeriesCollection
 
         private IEnumerable<List<double>> PointsGroups
         {
-            get { return Enumerable.Range(0, _offsets.Length).Select(_ => new List<double>()).ToList(); }
+            get { return Enumerable.Range(0, _offsets.Count).Select(_ => new List<double>()).ToList(); }
         }
 
         private bool SeriesHasIndex(int index)
