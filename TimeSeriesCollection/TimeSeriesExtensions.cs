1a0a9f9e3112e13dad438c6616c2a3d76a8810eb
Roman Shmatov <romanshmatov@gmail.com>
2014-04-28 20:42:43 +0600

    Итеративный алгоритм вычисления добавки. Небольшой рефакторинг.

--- TimeSeriesCollection/TimeSeriesExtensions.cs
+++ TimeSeriesCollection/TimeSeriesExtensions.cs
@@ -6,10 +6,23 @@ namespace TimeSeriesCollection
 {
     public static class TimeSeriesExtensions
     {
+        public class Addition
+        {
+            public int Radius;
+            public IEnumerable<double> Series;
+
+            public Addition(IEnumerable<double> series, int radius)
+            {
+                Series = series;
+                Radius = radius;
+            }
+        }
+
         public static IEnumerable<double?> Average(this IEnumerable<double?> series, int windowSize = 3)
         {
             return new SMACalculator(series, windowSize).Calculate();
         }
+
 
         public static IEnumerable<T> Truncate<T>(this IEnumerable<T> series, Func<T, bool> predicate)
         {
@@ -17,7 +30,7 @@ namespace TimeSeriesCollection
             var enumerator = series.SkipWhile(predicate).GetEnumerator();
             while (enumerator.MoveNext())
             {
-                var current = enumerator.Current;
+                T current = enumerator.Current;
                 if (predicate.Invoke(current))
                 {
                     buffer.Enqueue(current);
@@ -33,6 +46,25 @@ namespace TimeSeriesCollection
             }
         }
 
+
+        public static IEnumerable<double> Interpolate(this IEnumerable<double?> series)
+        {
+            return new Interpolator(series).Calculate();
+        }
+
+
+        /// <summary>
+        ///     Вычисление добавочной функции для особых точек в заданном радиусе.
+        /// </summary>
+        public static Addition CalculateF(this IEnumerable<double> series, IEnumerable<int> pointsIndices,
+            int radius)
+        {
+            var average =
+                new SpecialPointsAverageCalculator(series, pointsIndices, radius).Calculate();
+            return new Addition(average, radius);
+        }
+
+
         private static Func<int, double, double> BuildWeightFunction(int radius)
         {
             return (i, x) =>
@@ -42,30 +74,27 @@ namespace TimeSeriesCollection
             };
         }
 
-        public static IEnumerable<double> Weigh(this IEnumerable<double> series, int radius = 3)
+
+        /// <summary>
+        ///     Домножает добавку на "колокольчик".
+        /// </summary>
+        public static Addition Weight(this Addition addition)
         {
-            var f = BuildWeightFunction(radius);
-            return series.Select((value, index) => f(index, value));
+            var f = BuildWeightFunction(addition.Radius);
+            var weighted = addition.Series.Select((value, index) => f(index, value));
+            return new Addition(weighted, addition.Radius);
         }
 
-        public static IEnumerable<double> Interpolate(this IEnumerable<double?> series)
-        {
-            return new Interpolator(series).Calculate();
-        }
 
-        public static Addition CalculateF(this IEnumerable<double> series, IEnumerable<int> specialPointsIndices,
-            int radius = 3)
-        {
-            var average =
-                new SpecialPointsAverageCalculator(series, specialPointsIndices, radius).Calculate();
-            return new Addition(average, radius);
-        }
-
-        public static IEnumerable<double> Add(this IEnumerable<double> series, Addition f, int[] specialPointsIndices)
+        /// <summary>
+        ///     Добавление к ряду добавки в заданных точках.
+        /// </summary>
+        public static IEnumerable<double> Add(this IEnumerable<double> series, Addition f,
+            IList<int> pointsIndices)
         {
             var listSeries = series.ToList();
             var offsets = Enumerable.Range(-1*f.Radius, 2*f.Radius + 1);
-            var additions = specialPointsIndices.SelectMany(x => offsets.Select(offset => x + offset)
+            var additions = pointsIndices.SelectMany(x => offsets.Select(offset => x + offset)
                 .Where(index => index >= 0 && index < listSeries.Count)
                 .Zip(f.Series, (index, value) => new {index, value}));
 
@@ -74,37 +103,42 @@ namespace TimeSeriesCollection
             return listSeries;
         }
 
-        public static Addition CalculateAddition(this IEnumerable<double> series, IEnumerable<int> specialPointsIndices,
-            int radius = 3)
+
+        /// <summary>
+        ///     Итеративный алгоритм вычисления взвешенных добавок.
+        /// </summary>
+        public static IEnumerable<IEnumerable<Addition>> CalculateAdditions(this IEnumerable<double> s,
+            IEnumerable<IEnumerable<int>> pointsIndices, int radius, double weight)
         {
-            const int iterations = 5;
-            const double weight = 0.2;
+            var indicesGroups = pointsIndices.Select(x => x.ToList()).ToList();
+            var series = s.ToList();
 
-            var result = Enumerable.Repeat((double) 0, radius*2 + 1);
-            var seriesList = series.ToList();
-            var specialPointsIndicesArray = specialPointsIndices.ToArray();
-
-            var i = 0;
-            while (i++ < iterations)
+            while (true)
             {
-                var weighted = seriesList.CalculateF(specialPointsIndicesArray, radius).Series
-                    .Select(x => x*weight).ToList();
-                result = result.Zip(weighted, (res, add) => res + add);
-                seriesList.Add(new Addition(weighted, radius), specialPointsIndicesArray);
+                var seriesAddition = Enumerable.Repeat<double>(0, series.Count);
+                var additions = new List<Addition>();
+                foreach (var indices in indicesGroups)
+                {
+                    var addition = series.CalculateAddition(indices, radius, weight);
+                    additions.Add(addition);
+                    seriesAddition = seriesAddition.Add(addition, indices);
+                }
+                series = series.Zip(seriesAddition, (x, a) => x + a).ToList();
+                yield return additions;
             }
-            return new Addition(result, radius);
         }
 
-        public class Addition
-        {
-            public int Radius;
-            public IEnumerable<double> Series;
 
-            public Addition(IEnumerable<double> series, int radius)
-            {
-                Series = series;
-                Radius = radius;
-            }
+        /// <summary>
+        ///     Вычисление взвешенной добавки.
+        /// </summary>
+        public static Addition CalculateAddition(this IEnumerable<double> series,
+            IEnumerable<int> pointsIndices, int radius, double weight)
+        {
+            var weighted = series.CalculateF(pointsIndices, radius)
+                .Weight()
+                .Series.Select(x => x*weight);
+            return new Addition(weighted, radius);
         }
     }
 }
--- TimeSeriesCollectionTests/TimeSeriesExtensionsTests.cs
+++ TimeSeriesCollectionTests/TimeSeriesExtensionsTests.cs
@@ -11,28 +11,29 @@ namespace TimeSeriesCollectionTests
     namespace TimeSeriesExtensionsTests
     {
         [TestFixture]
-        class Truncate
+        internal class Truncate
         {
             [Test]
             public void ShouldTruncate()
             {
-                Assert.AreEqual(new double?[]{1, null,  1, 1},
-                    new double? [] {null, null, 1, null, 1, 1, null, null}.Truncate(x => !x.HasValue));
+                Assert.AreEqual(new double?[] {1, null, 1, 1},
+                    new double?[] {null, null, 1, null, 1, 1, null, null}.Truncate(x => !x.HasValue));
             }
         }
 
         [TestFixture]
-        class Weigh
+        internal class Weigh
         {
             [TestCase(2, new double[] {0, 1, 2, 1, 0}, new double[] {2, 2, 2, 2, 2})]
-            public void ShouldWeigh(int radius, double [] expected, double[] actual)
+            public void ShouldWeigh(int radius, double[] expected, double[] actual)
             {
-                Assert.AreEqual(expected, actual.Weigh(radius));
+                var addition = new TimeSeriesExtensions.Addition(actual, radius);
+                Assert.AreEqual(expected, addition.Weight().Series);
             }
         }
 
         [TestFixture]
-        class CalculateF
+        internal class CalculateF
         {
             [Test]
             public void ShouldCalculateF()
@@ -44,7 +45,7 @@ namespace TimeSeriesCollectionTests
         }
 
         [TestFixture]
-        class Add
+        internal class Add
         {
             [Test]
             public void ShouldAdd()
@@ -57,16 +58,14 @@ namespace TimeSeriesCollectionTests
         }
 
         [TestFixture]
-        class CalculateAddition
+        internal class CalculateAdditions
         {
-            [Test]
-            public void ShouldCalculateAddition()
+            [TestCase(new double[] { 1, 2, 3, 2, 1, 3, 4, 5, 4, 3 }, new[] { 2, 7 }, new double[] { 0, 1.5, 4, 1.5, 0 })]
+            public void ShouldCalculateAdditions(IEnumerable<double> series, IEnumerable<int> points, IEnumerable<double> expected)
             {
-                var series = new double[] { 1, 2, 3, 2, 1, 3, 4, 5, 4, 3 };
-                var expected = new double[] { 2, 3, 4, 3, 2 };
-                Assert.AreEqual(expected, series.CalculateF(new[] { 2, 7 }, 2).Series);
+                var actual = series.CalculateAddition(points, 2, 1).Series;
+                Assert.AreEqual(expected, actual);
             }
-
         }
     }
 }
