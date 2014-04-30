d48814acf2da5203cbfbaa2f1f57edd89862579b
Roman Shmatov <romanshmatov@gmail.com>
2014-04-30 16:40:06 +0600

    Интерфейс EMAForecaster

---
+++ TimeSeriesCollection/EMAForecaster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSeriesCollection
+{
+    class EMAForecaster
+    {
+        private readonly IEnumerable<double> _series;
+
+        public EMAForecaster(IEnumerable<double> series)
+        {
+            _series = series;
+        }
+
+        public IEnumerable<double> Forecast(int period)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
